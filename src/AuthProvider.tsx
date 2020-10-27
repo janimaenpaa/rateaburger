4f365d13407a177a021ee853b6f25db056f7ca80
janimaenpaa <itsmayd@gmail.com>
2020-10-28 00:46:07 +0200

    Add AuthProvider

---
+++ src/AuthProvider.tsx
@@ -0,0 +1,37 @@
+import React, { useState } from "react"
+import AsyncStorage from "@react-native-community/async-storage"
+
+type User = null | { email: string }
+
+export const AuthContext = React.createContext<{
+  user: User
+  login: () => void
+  logout: () => void
+}>({
+  user: null,
+  login: () => {},
+  logout: () => {},
+})
+
+interface AuthProviderProps {}
+
+export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+  const [user, setUser] = useState<User>(null)
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        login: () => {
+          const testUser = { email: "matti@abc.com" }
+          setUser(testUser)
+          AsyncStorage.setItem("user", JSON.stringify(testUser))
+        },
+        logout: () => {
+          AsyncStorage.removeItem("user")
+        },
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  )
+}
