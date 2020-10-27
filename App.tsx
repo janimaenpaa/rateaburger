6ab60cb0844f7d4c64502c102ffb81a9270ac9bb
janimaenpaa <itsmayd@gmail.com>
2020-10-28 01:14:38 +0200

    Add test authentication to Routes.tsx, add Context Provider to App.tsx

--- App.tsx
+++ App.tsx
@@ -1,3 +1,15 @@
+import React from "react"
+import { AuthProvider } from "./src/AuthProvider"
 import { Routes } from "./src/Routes"
 
-export default Routes
+interface AppProps {}
+
+const App: React.FC<AppProps> = () => {
+  return (
+    <AuthProvider>
+      <Routes />
+    </AuthProvider>
+  )
+}
+
+export default App
--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,24 +1,24 @@
-import React from "react"
-import {
-  createStackNavigator,
-  StackNavigationProp,
-} from "@react-navigation/stack"
-import { NavigationContainer, RouteProp } from "@react-navigation/native"
-import { Button, Text } from "react-native"
+import React, { useState, useEffect, useContext } from "react"
+import { createStackNavigator } from "@react-navigation/stack"
+import { NavigationContainer } from "@react-navigation/native"
+import { ActivityIndicator, Button, Text } from "react-native"
+import AsyncStorage from "@react-native-community/async-storage"
 import { Center } from "./Center"
 import { AuthNavProps, AuthParamList } from "./types"
-
-interface RoutesProps {}
+import { AuthContext } from "./AuthProvider"
 
 const Stack = createStackNavigator<AuthParamList>()
 
-const Login = ({ navigation }: AuthNavProps<"Login">) => (
-  <Center>
-    <Text>Login</Text>
-    <Button title="Signup" onPress={() => navigation.navigate("Signup")} />
-  </Center>
-)
-
+const Login = ({ navigation }: AuthNavProps<"Login">) => {
+  const { login } = useContext(AuthContext)
+  return (
+    <Center>
+      <Text>Login</Text>
+      <Button title="Log me in" onPress={() => login()} />
+      <Button title="Signup" onPress={() => navigation.navigate("Signup")} />
+    </Center>
+  )
+}
 const Signup = ({ navigation }: AuthNavProps<"Signup">) => (
   <Center>
     <Text>Signup</Text>
@@ -26,17 +26,53 @@ const Signup = ({ navigation }: AuthNavProps<"Signup">) => (
   </Center>
 )
 
+interface RoutesProps {}
+
 export const Routes: React.FC<RoutesProps> = ({}) => {
+  const { user, login } = useContext(AuthContext)
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    AsyncStorage.getItem("user")
+      .then((user) => {
+        console.log(user)
+        if (user) {
+          //do something
+          login()
+          setLoading(false)
+        } else {
+          setLoading(false)
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+  if (loading) {
+    return (
+      <Center>
+        <ActivityIndicator size="large" />
+      </Center>
+    )
+  }
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          options={{ headerShown: false }}
-          component={Login}
-        />
-        <Stack.Screen name="Signup" component={Signup} />
-      </Stack.Navigator>
+      {user ? (
+        <Center>
+          <Text>User exists</Text>
+        </Center>
+      ) : (
+        <Stack.Navigator initialRouteName="Login">
+          <Stack.Screen
+            name="Login"
+            options={{ headerShown: false }}
+            component={Login}
+          />
+          <Stack.Screen name="Signup" component={Signup} />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   )
 }
