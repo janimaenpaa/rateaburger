b4eb0e86de6a7621c1ab561e3005fc7f397bd488
janimaenpaa <itsmayd@gmail.com>
2020-10-30 16:00:48 +0200

    Add Bottom Tab icons

--- src/AppTabs.tsx
+++ src/AppTabs.tsx
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { Center } from "./Center"
 import { Button, Text } from "react-native"
 import { AuthContext } from "./AuthProvider"
+import { Ionicons } from "@expo/vector-icons"
+import { FontAwesome, FontAwesome5 } from "@expo/vector-icons"
 
 interface AppTabsProps {}
 
@@ -33,6 +35,22 @@ const Burgers = () => (
 export const AppTabs: React.FC<AppTabsProps> = ({}) => {
   return (
     <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName = ""
+
+          if (route.name === "Home") {
+            return <FontAwesome name="home" size={24} color="black" />
+          } else if (route.name === "Burgers") {
+            return <FontAwesome5 name="hamburger" size={24} color="black" />
+          } else if (route.name === "Map") {
+            return <FontAwesome name="map" size={24} color="black" />
+          }
+
+          // You can return any component that you like here!
+          return <Ionicons name={iconName} size={size} color={color} />
+        },
+      })}
       tabBarOptions={{
         tabStyle: {
           alignSelf: "center",
