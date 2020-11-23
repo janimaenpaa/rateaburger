3a6f306b3ae1396d22107dbe22b2b8fed1d7bfb5
janimaenpaa <itsmayd@gmail.com>
2020-11-23 23:53:35 +0200

    Add List to Restaurants page, add statusbarheight to layout margin top

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,18 @@
 import React from "react"
 import { Layout as KittenUILayout } from "@ui-kitten/components"
+import Constants from "expo-constants"
 
 interface CenterProps {}
 
 export const Layout: React.FC<CenterProps> = ({ children }) => {
   return (
     <KittenUILayout
-      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        marginTop: Constants.statusBarHeight,
+      }}
     >
       {children}
     </KittenUILayout>
--- src/stacks/AppTabs/Restaurants.tsx
+++ src/stacks/AppTabs/Restaurants.tsx
@@ -1,7 +1,9 @@
 import React, { useContext } from "react"
 import { Layout } from "../../components/Layout"
-import { Text } from "react-native"
+import { Button, Text } from "react-native"
 import { DataContext } from "../../providers/DataProvider"
+import { List, ListItem, ListItemProps } from "@ui-kitten/components"
+import { Restaurant } from "../../types"
 
 interface RestaurantsProps {}
 
@@ -11,6 +13,13 @@ export const Restaurants: React.FC<RestaurantsProps> = () => {
   return (
     <Layout>
       <Text>Restaurants</Text>
+      <List
+        style={{ width: "100%" }}
+        data={restaurants}
+        renderItem={({ item }: { item: Restaurant }) => (
+          <ListItem key={item.id} title={item.name} description={item.address} />
+        )}
+      />
     </Layout>
   )
 }
