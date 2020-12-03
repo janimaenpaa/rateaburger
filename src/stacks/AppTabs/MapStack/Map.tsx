30bb154240eac59d3ea58c46127b3ef6f58292c4
janimaenpaa <itsmayd@gmail.com>
2020-12-03 16:55:54 +0200

    Add restaurant markers to Map

--- src/stacks/AppTabs/MapStack/Map.tsx
+++ src/stacks/AppTabs/MapStack/Map.tsx
@@ -1,17 +1,19 @@
-import React, { useEffect, useState } from "react"
+import React, { useContext, useEffect, useState } from "react"
 import { Container } from "../../../components/Container"
 import { StyleSheet, Alert } from "react-native"
-import MapView from "react-native-maps"
+import MapView, { Marker } from "react-native-maps"
 import * as Location from "expo-location"
+import { DataContext } from "../../../providers/DataProvider"
 
 interface MapProps {}
 
 export const Map: React.FC<MapProps> = () => {
+  const { restaurants } = useContext(DataContext)
   const [region, setRegion] = useState({
     latitude: 60.169877007627676,
     longitude: 24.938366007081427,
     latitudeDelta: 0.03,
-    longitudeDelta: 0.02,
+    longitudeDelta: 0.03,
   })
 
   const getPhoneLocation = async () => {
@@ -38,9 +40,22 @@ export const Map: React.FC<MapProps> = () => {
     getPhoneLocation()
   }, [])
 
+  const markers = restaurants.map((restaurant) => (
+    <Marker
+      key={restaurant.id}
+      coordinate={{
+        latitude: Number(restaurant.coordinates.latitude),
+        longitude: Number(restaurant.coordinates.longitude),
+      }}
+      title={restaurant.name}
+    />
+  ))
+
   return (
     <Container>
-      <MapView style={styles.mapStyle} region={region} />
+      <MapView style={styles.mapStyle} region={region}>
+        {markers}
+      </MapView>
     </Container>
   )
 }
