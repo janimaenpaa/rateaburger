c6de76fb8ea83109b328e23fa0a4bf5ce8c38783
janimaenpaa <itsmayd@gmail.com>
2020-12-06 02:00:20 +0200

    Make AddRestaurant make proper post requests

--- src/stacks/AppTabs/RestaurantStack/AddRestaurant.tsx
+++ src/stacks/AppTabs/RestaurantStack/AddRestaurant.tsx
@@ -1,17 +1,98 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { useForm, Controller } from "react-hook-form"
-import { Alert, StyleSheet } from "react-native"
+import { Alert, StyleSheet, Image } from "react-native"
 import { Text, Input, Button, Layout } from "@ui-kitten/components"
 import { Container } from "../../../components/Container"
 import { RestaurantNavProps } from "../../../types"
+import * as ImagePicker from "expo-image-picker"
 
-type FormData = {}
+declare global {
+  interface FormDataValue {
+    uri: string
+    name: string
+    type: string
+  }
+
+  interface FormData {
+    append(name: string, value: FormDataValue, fileName?: string): void
+    set(name: string, value: FormDataValue, fileName?: string): void
+  }
+}
 
 export const AddRestaurant = ({
   navigation,
 }: RestaurantNavProps<"AddRestaurant">) => {
+  const [image, setImage] = useState<any>(null)
+  const [uploadedImgUrl, setUploadedImgUrl] = useState<string | null>("")
   const { control, handleSubmit, errors } = useForm()
-  const onSubmit = (data: any) => console.log(data)
+  const onSubmit = (data: any) => {
+    const restaurant = { ...data, imgUrl: uploadedImgUrl }
+    console.log(restaurant)
+
+    fetch("https://rateaburger.herokuapp.com/api/restaurants", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(restaurant),
+    })
+      .then((response) => response.json())
+      .then((json) => {
+        console.log({ response: json })
+        navigation.goBack()
+      })
+      .catch((error) => console.log(error))
+  }
+
+  const noImg =
+    "https://rateaburger.s3.eu-north-1.amazonaws.com/7164666a-a1f0-494a-a9c0-bb669f2d0195"
+
+  useEffect(() => {
+    ;async () => {
+      const { status } = await ImagePicker.requestCameraRollPermissionsAsync()
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!")
+      }
+    }
+  }, [])
+
+  const handleImageUpload = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [16, 9],
+      quality: 1,
+    })
+
+    console.log(result)
+
+    if (!result.cancelled) {
+      setImage(result.uri)
+      const formData = new FormData()
+
+      const randomName = Math.floor(Math.random() * 1000000)
+
+      formData.append("file", {
+        uri: result.uri,
+        type: "image/jpeg",
+        name: String(randomName + ".jpg"),
+      })
+
+      console.log(formData)
+
+      fetch("https://rateaburger.herokuapp.com/api/upload", {
+        method: "POST",
+        body: formData,
+      })
+        .then((response) => response.json())
+        .then((json) => {
+          console.log(json)
+          setUploadedImgUrl(json.imgUrl)
+        })
+        .catch((error) => console.log(error))
+    }
+  }
+
   return (
     <Container>
       <Layout style={styles.layout}>
@@ -66,22 +147,14 @@ export const AddRestaurant = ({
           defaultValue=""
         />
         {errors.address && <Text>Restaurant name is required.</Text>}
-        <Controller
-          control={control}
-          render={({ onChange, onBlur, value }) => (
-            <Input
-              label="Add Image"
-              style={styles.input}
-              onBlur={onBlur}
-              onChangeText={(value) => onChange(value)}
-              value={value}
-            />
-          )}
-          name="imgUrl"
-          rules={{ required: true }}
-          defaultValue=""
-        />
-        {errors.address && <Text>Restaurant name is required.</Text>}
+        {image ? (
+          <Image source={{ uri: image }} style={styles.image} />
+        ) : (
+          <Image source={{ uri: noImg }} style={styles.image} />
+        )}
+        <Button status="info" onPress={handleImageUpload}>
+          Add Image
+        </Button>
         <Button style={styles.button} onPress={handleSubmit(onSubmit)}>
           Submit
         </Button>
@@ -100,5 +173,12 @@ const styles = StyleSheet.create({
   layout: {
     margin: 20,
     backgroundColor: "#F7F9FC",
+  },
+  image: {
+    width: "100%",
+    height: 200,
+    marginTop: 10,
+    marginBottom: 10,
+    borderRadius: 8,
   },
 })
